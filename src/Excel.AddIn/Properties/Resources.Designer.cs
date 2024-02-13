818866f8db75629254e92bc2c49b3a2c8c8d6777
hdmun <hdmun@kakao.com>
2024-02-13 14:07:17 +0900

    [FIX] missing rename to `Excel.AddIn`

--- src/Excel.AddIn/Properties/Resources.Designer.cs
+++ src/Excel.AddIn/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FlatbufferSerializer.Properties {
+namespace Excel.AddIn.Properties {
     
     
     /// <summary>
--- src/Excel.AddIn/ThisAddIn.cs
+++ src/Excel.AddIn/ThisAddIn.cs
@@ -7,7 +7,7 @@ using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
 
-namespace FlatbufferSerializer
+namespace Excel.AddIn
 {
     public partial class ThisAddIn
     {
