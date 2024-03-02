95db0b4e045490b20aa197ccca9feffb9276c5ad
hdmun <hdmun@kakao.com>
2024-03-02 13:41:40 +0900

    [FEAT] flatc 를 사용해 flatbuffer 클래스 파일 출력
    https://github.com/hdmun/fbs-excel-addin/issues/1

--- src/Excel.AddIn/SchemaWriter.cs
+++ src/Excel.AddIn/SchemaWriter.cs
@@ -1,4 +1,5 @@
 ﻿using Flatbuffer.Serializer.Schema;
+using System.Diagnostics;
 using System.IO;
 
 namespace Excel.AddIn
@@ -13,6 +14,10 @@ namespace Excel.AddIn
         {
             var path = $"{table.Name}.fbs";
             File.WriteAllText(path, table.ToSchemaText());
+
+            // 클래스 파일 출력
+            var flatc = "flatc.exe";
+            Process.Start(flatc, $"--csharp {path}");
         }
     }
 }
